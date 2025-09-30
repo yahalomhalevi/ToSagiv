d4bcc01c1f76b996a77462a64ec2d50f7375e144
yahalomhalevy <y0548482954@gmail.com>
2025-09-30 12:54:49 +0300

    style: improve UI with loader animation, card design and responsive form

--- js/script.js
+++ js/script.js
@@ -6,7 +6,16 @@ $(document).ready(function () {
             $('#result').html('<div class="alert alert-warning"> אנא הזן מספר רכב <div/>');
             return;
         }
-        $('#result').html('<div class="text-center mt-3">  טוען מידע ...</div>');
+
+//  Loder עם אנימציית ספינר של bootsrap 
+        $('#result').html(`
+            <div class="d-flex justify-content-center my-4">
+            <div class="spinner-border text-primary" role="status">
+            <span class="visually-hidden">טוען...>/span>
+            </div>
+        </div>
+      `);
+
         const resourceId = "053ea72c-0ea2-4f6c-8fca-7896b3059a27";
         const apiUrl =
             `https://data.gov.il/api/3/action/datastore_search?resource_id=${resourceId} & q=${carNumber}`;
@@ -15,13 +24,13 @@ $(document).ready(function () {
             if (data.success && data.result.records.length > 0) {
                 const car = data.result.records[0]; // שנמצא הראשון הרכב
                 let output = `
-                < div class= "card" >
-                    <div class="card-header bg-primary text-white">
-                    רכב פרטי: $}car.mispar_rechev}
+             < div class= "card shadow-sm mt-4">
+                    <div class="card-header bg-primary text-white fs-5 fw-bold">
+                    פרטי רכב: $}car.mispar_rechev}
                 </div>
                 <div class="card-body">
                    <p><strong>תיאור:>/strong> ${car.tozeret_nm}
-                ${car.kinuy_mishari}</p>
+                    ${car.kinuy_mishari}</p>
                  <p><strong>רכב סוג:>/strong> ${car.sug_delek_nm}</p>
                  <p><strong>ייצור שנת:>/strong> ${car.shnat_yitzur}</p>
                  <p><strong>רישיון תוקף:>/strong> ${car.tokef_dt}</p>
@@ -31,10 +40,10 @@ $(document).ready(function () {
                 $('#result').html(output);
             } else {
                 $('#result').html('<div class="alert alert-danger mt-3"> לא נמצאו פרטים עבור מספר זה</div>');
- }
+            }
         }).fail(function () {
             $('#result').html('<div class="alert alert-danger mt-3">  אירעה שגיאה בעת שליפת הנתונים<div/>');
- });
+        });
     });
 });
 
